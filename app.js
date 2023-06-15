8284c9271310a8140730ef9f786cb2b4ecc89d60
unknown <araf.chakib.m@gmail.com>
2023-06-15 02:06:33 +0200

    features creation du cookie

--- app.js
+++ app.js
@@ -9,9 +9,26 @@ inputs.forEach(input => {
 
 function handleValidation(e){
    if(e.type === "invalid"){
-    e.target.setCustomValidity("ce champ ne peut etre vide")
+    e.target.setCustomValidity("ce champ ne peut etre vide");
    }
    else if (e.type === "input"){
-    e.target.setCustomValidity("")
-   }
+    e.target.setCustomValidity("");
+}
+}
+
+const cookieForm = document.querySelector("form");
+cookieForm.addEventListener("submit", handleForm);
+
+function handleForm(e){
+    e.preventDefault()
+
+    const newCookie = {};
+    
+    inputs.forEach(input => {
+        const nameAttribute = input.getAttribute("name")
+        newCookie[nameAttribute]= input.value;
+
+    })
+
+    console.log(newCookie);
 }
