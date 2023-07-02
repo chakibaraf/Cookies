354dc44f55528a75b0ecb00733cbae4b89eed86c
unknown <araf.chakib.m@gmail.com>
2023-07-02 20:46:13 +0200

    creation du cookie

--- app.js
+++ app.js
@@ -2,37 +2,67 @@ const inputs = document.querySelectorAll("input");
 
 
 inputs.forEach(input => {
-        input.addEventListener("invalid", handleValidation);
-        input.addEventListener("input", handleValidation);
+    input.addEventListener("invalid", handleValidation);
+    input.addEventListener("input", handleValidation);
 
-    });
+});
 
-function handleValidation(e){
-   if(e.type === "invalid"){
-    e.target.setCustomValidity("ce champ ne peut etre vide");
-   }
-   else if (e.type === "input"){
-    e.target.setCustomValidity("");
-}
+function handleValidation(e) {
+    if (e.type === "invalid") {
+        e.target.setCustomValidity("ce champ ne peut etre vide");
+    }
+    else if (e.type === "input") {
+        e.target.setCustomValidity("");
+    }
 }
 
 const cookieForm = document.querySelector("form");
 cookieForm.addEventListener("submit", handleForm);
 
-function handleForm(e){
+function handleForm(e) {
     e.preventDefault()
 
     const newCookie = {};
 
     inputs.forEach(input => {
         const nameAttribute = input.getAttribute("name")
-        newCookie[nameAttribute]= input.value;
+        newCookie[nameAttribute] = input.value;
 
     })
-    newCookie.expires = new Date (new Date().getTime()+ 7 * 24 * 60 * 60 * 1000)
+    newCookie.expires = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000)
 
-createCookie(newCookie)
+    // console.log(newCookie);
+    createCookie(newCookie)
 }
 
 
-function createCookie(newCookie)
+function createCookie(newCookie) {
+
+    if (doesCookieExist(newCookie.name)) {
+        createToast({ name: newCookie.name, state: "modifié", color: "organised" })
+    }
+    else {
+        createToast({ name: newCookie.name, state: "crée", color: "green" })
+    }
+
+    document.cookie = `${encodeURIComponent(newCookie.name)}=${encodeURIComponent(newCookie.value)};expires=${newCookie.expires.
+        toUTCString()}`
+
+}
+
+function doesCookieExist(name) {
+    const cookies = document.cookie.replace(/\s/g, "").split(";");
+    const onlyCookiesName = cookies.map(cookie => cookie.split("=")[0])
+    console.log(cookies, onlyCookiesName);
+    const cookiePresence = onlyCookiesName.find(cookie => cookie === encodeURIComponent(name))
+    return cookiePresence;
+}
+
+const toastContainer = document.querySelector(".toasts-container")
+function createToast({name,state,color}){
+    const tostInfo = document.createElement("p");
+    tostInfo.className = "toast";
+
+    tostInfo.textContent = `Cookie ${name} ${state}`;
+    tostInfo.style.background = color;
+}
